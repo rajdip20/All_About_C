d72b88fd2ff1d868f5e834174ac83f04218f3f9f
rajdip20 <dasrajdip78754282@gmail.com>
2021-07-05 08:10:18 +0530

    Initial commit

---
+++ College_LAB_2nd_SEM/Prg_43c.c
@@ -0,0 +1,33 @@
+/*
+    Assignment on numerical solution of algebraic equation by Bisection method.
+
+    ~~~~~~ Update Date : 05/07/2021 ~~~~~~
+*/
+
+#include<stdio.h>
+#include<math.h>
+#define f(x) ((x * x * x) - x - 1)
+int main()
+{
+    float a, b, x0;
+    float e = 0.0001;
+    do
+    {
+        printf("Enter the value of a and b: ");
+        scanf("%f %f", &a, &b);
+    } while (f(a) * f(b) > 0);
+
+    do
+    {
+        x0 = (a + b) / 2;
+        if (f(a) * f(x0) < 0)
+        {
+            b = x0;
+        }
+        else
+        {
+            a = x0;
+        }
+    } while (fabs(a - b) > e);
+    printf("The root is %f", x0);
+}

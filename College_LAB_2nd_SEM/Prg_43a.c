96b812373a64d426c0fb82ec080573cc56493276
rajdip20 <dasrajdip78754282@gmail.com>
2021-07-05 08:08:27 +0530

    Initial commit

---
+++ College_LAB_2nd_SEM/Prg_43a.c
@@ -0,0 +1,33 @@
+/*
+    Assignment on numerical solution of algebraic equation by Regula-falsi method.
+
+    ~~~~~~ Update Date : 05/07/2021 ~~~~~~
+*/
+
+#include<stdio.h>
+#include<math.h>
+#define f(x) (3 * (x * x * x) - 9 * (x * x) + 8)
+int main()
+{
+    float a, b, x0;
+    float e = 0.0001;
+    do
+    {
+        printf("Enter the value of a and b: ");
+        scanf("%f %f", &a, &b);
+    } while (f(a) * f(b) > 0);
+
+    do
+    {
+        x0 = (a * f(b) - b * f(a)) / (f(b) - f(a));
+        if (f(a) * f(x0) < 0)
+        {
+            b = x0;
+        }
+        else
+        {
+            a = x0;
+        }
+    } while (fabs(a - b) > e);
+    printf("The root is %f", x0);
+}

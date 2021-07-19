bd32031c3e7a3540c396e77dcf590fd58eabb7ae
rajdip20 <dasrajdip78754282@gmail.com>
2021-07-19 07:24:09 +0530

    Initial commit

--- College_LAB_2nd_SEM/Prg_12b.c
+++ College_LAB_2nd_SEM/Prg_12b.c
@@ -2,7 +2,7 @@
     Write a program in c to find the sum of the following series.
         s= 1+3+5+7+...+n(input n).
 
-    ~~~~~~ Update Date : 01/07/2021 ~~~~~~
+    ~~~~~~ Update Date : 02/07/2021 ~~~~~~
 */
 
 #include<stdio.h>
